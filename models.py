7ffd6d1263c0d72b96ab169ef988e28c0657257c
haja <haja@student.chalmers.se>
2025-08-03 16:12:56 +0200

    Make the name of the product unique to prevent having duplication

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True) # Every product will have a unique ID
-    name = db.Column(db.String(100), nullable=False) # Product name (required)
+    name = db.Column(db.String(100), unique=True, nullable=False) # Product name (required)
     category = db.Column(db.String(50), nullable=True) # Product category (optional)
     stock = db.Column(db.Integer, nullable=False) # Quantity in stock (required)
     price = db.Column(db.Float, nullable=False) # Product price (required)
